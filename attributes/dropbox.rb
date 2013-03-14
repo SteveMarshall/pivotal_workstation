62718c4beade036897ad79c214dfa17c333da2f0
Brian Cunnie <pair+cunnie@pivotallabs.com>
2013-03-14 15:01:48 -0700

    Dropbox 2.0.0

--- attributes/dropbox.rb
+++ attributes/dropbox.rb
@@ -1,3 +1,3 @@
 node.default["dropbox"]["source"]="https://www.dropbox.com/download?plat=mac"
-node.default["dropbox"]["checksum"]="e80f8974b1759f85f98c43c5ba8784f400ff9da507cf73077c2366c15bb04e33"
+node.default["dropbox"]["checksum"]="d74666cdb67ac7c82aedfaf8654bfc77c65fc9596d2a346ab08ec5c5cb4f735a"
 node.default["dropbox"]["destination"]="/Applications"
